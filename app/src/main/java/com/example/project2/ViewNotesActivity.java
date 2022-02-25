31b04305e74d9f07df7d2a376d4e6142f30fc790
quenchglo <qianw01@gmail.com>
2022-02-25 11:26:55 -0500

    added some pages

---
+++ app/src/main/java/com/example/project2/ViewNotesActivity.java
@@ -0,0 +1,14 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_notes);
+    }
+}
