1ae13292db7f88090b05c717377b2a2c0078a5a9
quenchglo <qianw01@gmail.com>
2022-03-03 11:47:57 -0500

    updated register stuff

--- app/src/main/java/com/example/project2/LoginActivity.java
+++ app/src/main/java/com/example/project2/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button logSubmitBtn = findViewById(R.id.logSubmitBtn);
+        TextView usernameLog = findViewById(R.id.usernameLog);
+        TextView passLog = findViewById(R.id.passLog);
+
+        logSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameLog.getText().toString();
+                String password = passLog.getText().toString();
+
+                if (username.selectInfoQuery(username)) {
+
+                }
+                Intent nextInt = new Intent(getApplicationContext(), HomepageActivity.class);
+                startActivity(nextInt);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/project2/RegisterActivity.java
+++ app/src/main/java/com/example/project2/RegisterActivity.java
@@ -2,10 +2,13 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -18,13 +21,30 @@ public class RegisterActivity extends AppCompatActivity {
         TextView userText = findViewById(R.id.usernameReg);
         TextView passText = findViewById(R.id.passReg);
 
+        TextView nameReg = findViewById(R.id.nameReg);
+        TextView ageReg = findViewById(R.id.ageReg);
+        TextView genderReg = findViewById(R.id.genderReg);
+
+
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = userText.getText().toString();
                 String password = passText.getText().toString();
                 if (username.length() >= 5 && password.length() >= 8 && Character.isUpperCase(password.charAt(0))) {
+                    String name = nameReg.getText().toString();
+                    String age = ageReg.getText().toString();
+                    String gender = genderReg.getText().toString();
+                    DBClass db = new DBClass(getApplicationContext(), "userInfo");
+                    db.addUserInfo(name, age, gender, username, password);
+                    userText.setText("");
+                    passText.setText("");
+                    nameReg.setText("");
+                    ageReg.setText("");
+                    genderReg.setText("");
 
+                    Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(loginIntent);
                 }
             }
         });
